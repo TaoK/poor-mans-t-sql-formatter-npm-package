9e7287602326d82f3d0dcbb0a79616384b85c0a5
Tao Klerks <tao@klerks.biz>
2017-10-23 23:29:31 +0200

    Improve build process to be a little less error-prone, clarify structure

---
+++ build/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# This script creates the npm package file (index.js) from the current output in the main Poor Man's T-SQL Formatter project on github (master branch)
+
+#from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within?rq=1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cat "$DIR/prelude.txt" > "$DIR/../index.js" &&
+echo >> "$DIR/../index.js" &&
+wget -qO- https://raw.githubusercontent.com/TaoK/PoorMansTSqlFormatter/master/PoorMansTSqlFormatterWebDemo/JSLibReference/bridge.js >> "$DIR/../index.js" &&
+echo >> "$DIR/../index.js" &&
+wget -qO- https://raw.githubusercontent.com/TaoK/PoorMansTSqlFormatter/master/PoorMansTSqlFormatterWebDemo/JSLibReference/bridge.meta.js >> "$DIR/../index.js" &&
+echo >> "$DIR/../index.js" &&
+wget -qO- https://raw.githubusercontent.com/TaoK/PoorMansTSqlFormatter/master/PoorMansTSqlFormatterWebDemo/JSLibReference/PoorMansTSqlFormatterJS.js >> "$DIR/../index.js" &&
+echo >> "$DIR/../index.js" &&
+wget -qO- https://raw.githubusercontent.com/TaoK/PoorMansTSqlFormatter/master/PoorMansTSqlFormatterWebDemo/JSLibReference/PoorMansTSqlFormatterJS.meta.js >> "$DIR/../index.js" &&
+echo >> "$DIR/../index.js" &&
+cat "$DIR/suffix.txt" >> "$DIR/../index.js"
